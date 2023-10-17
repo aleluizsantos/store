dd78abe2e315519888dd47223e1673961f5c089a
Alessandro L. Santos <aleluizsantos@gmail.com>
2023-10-17 17:36:03 -0300

    first commit

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,60 @@
+import {
+  MenuIcon,
+  ShoppingCartIcon,
+  LogInIcon,
+  PercentIcon,
+  ListOrderedIcon,
+  HomeIcon,
+} from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+
+const Header = () => {
+  return (
+    <Card className="flex justify-between p-[1.87rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            MENU
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="ghost" className="w-full justify-start gap-2">
+              <LogInIcon size={16} /> Fazer Login
+            </Button>
+
+            <Button variant="ghost" className="w-full justify-start gap-2">
+              <HomeIcon size={16} /> Início
+            </Button>
+
+            <Button variant="ghost" className="w-full justify-start gap-2">
+              <PercentIcon size={16} /> Ofertas
+            </Button>
+
+            <Button variant="ghost" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} /> Catálago
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW </span>
+        Store
+      </h1>
+
+      <Button size="icon" variant="outline">
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
+  );
+};
+
+export default Header;
